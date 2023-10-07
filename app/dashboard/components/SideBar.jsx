28e64f36e033b3ec0b5778910b14effafe8a1fa6
krisnasaputtra <krisna.saputra031002@gmail.com>
2023-10-07 18:22:34 +0700

    Update

---
+++ app/dashboard/components/SideBar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SideBar() {
+  return <div className="w-2 h-full background-white-200">SideBar</div>;
+}
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,5 @@
+import Image from "next/image";
+
 export default function Dashboard() {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  );
+  return <h1>Dashboard</h1>;
 }
