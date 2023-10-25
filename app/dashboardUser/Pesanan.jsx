9779be1c6af0e7d5f3ea56090d352a4c8e4d52a4
siburianbasrunki <basrunkisiburian@gmail.com>
2023-10-25 19:37:03 +0700

    dashboardUser

---
+++ app/dashboardUser/Pesanan.jsx
@@ -0,0 +1,74 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Pesananimg from "../../public/Images/pesanan.png";
+
+const Pesanan = () => {
+  const [activeTab, setActiveTab] = useState("Riwayat"); // Menentukan tab aktif
+
+  // Fungsi untuk mengubah tab aktif
+  const changeTab = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  return (
+    <div className="mt-8">
+      <h1 className="font-bold text-3xl">Pesanan</h1>
+      <div className="flex mt-4">
+        <a
+          href="#"
+          className={`mr-4 ${activeTab === "Riwayat" ? "text-blue-500" : ""}`}
+          onClick={() => changeTab("Riwayat")}
+        >
+          Riwayat
+        </a>
+        <a
+          href="#"
+          className={`mr-4 ${
+            activeTab === "Dalam Proses" ? "text-blue-500" : ""
+          }`}
+          onClick={() => changeTab("Dalam Proses")}
+        >
+          Dalam Proses
+        </a>
+      </div>
+
+      {activeTab === "Riwayat" && (
+        <div className="flex justify-between items-center mt-4">
+          <div className="left flex items-center">
+            <div className="mr-4">
+              <Image src={Pesananimg} alt="gambar" className="rounded-lg" />
+            </div>
+            <div>
+              <p>Ayam Geprek Extra Tulang</p>
+              <small>Status Pesanan</small>
+            </div>
+          </div>
+          <div className="right">
+            <p>Rp 20.000</p>
+            <button className="btn">Pesan Lagi</button>
+          </div>
+        </div>
+      )}
+
+      {activeTab === "Dalam Proses" && (
+        <div className="flex justify-between items-center mt-4">
+          <div className="left flex items-center">
+            <div className="mr-4">
+              <Image src={Pesananimg} alt="gambar" className="rounded-lg" />
+            </div>
+            <div>
+              <p>Ayam Geprek Extra Tulang</p>
+              <small>Dalam Proses</small>
+            </div>
+          </div>
+          <div className="right">
+            <p>Rp 20.000</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Pesanan;
---
+++ app/dashboardUser/page.jsx
@@ -0,0 +1,113 @@
+"use client";
+import React, { useState } from "react";
+import Pesanan from "./Pesanan";
+import Alamat from "./Alamat";
+import AkunUser from "./Akun";
+
+const DashboardUser = () => {
+  const [selectedItem, setSelectedItem] = useState("Dashboard");
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const renderContent = () => {
+    switch (selectedItem) {
+      case "Dashboard":
+        return (
+          <div>
+            <h1>Dashboard</h1>
+            <p>Halo user</p>
+            <p>
+              Dari dashboard akun anda, anda dapat melihat pesanan terbaru,
+              mengedit alamat pengiriman, serta mengelola akun anda
+            </p>
+          </div>
+        );
+      case "Pesanan":
+        return <Pesanan />;
+      case "Akun":
+        return <AkunUser />;
+      case "Alamat":
+        return <Alamat />;
+      case "Keluar":
+        return (
+          <div>
+            <h1>Keluar</h1>
+            <p>Anda telah keluar dari akun Anda.</p>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+  return (
+    <div className="flex">
+      <div className="w-1/4  p-4">
+        <h1 className="text-2xl font-bold mb-4">Profil Pengguna</h1>
+        <ul>
+          <li className="mb-2">
+            <a
+              href="#"
+              onClick={() => handleItemClick("Dashboard")}
+              className={`cursor-pointer ${
+                selectedItem === "Dashboard" ? "underline" : ""
+              }`}
+            >
+              Dashboard
+            </a>
+          </li>
+          <li className="mb-2">
+            <a
+              href="#"
+              onClick={() => handleItemClick("Pesanan")}
+              className={`cursor-pointer ${
+                selectedItem === "Pesanan" ? "underline" : ""
+              }`}
+            >
+              Pesanan
+            </a>
+          </li>
+          <li className="mb-2">
+            <a
+              href="#"
+              onClick={() => handleItemClick("Akun")}
+              className={`cursor-pointer ${
+                selectedItem === "Akun" ? "underline" : ""
+              }`}
+            >
+              Akun
+            </a>
+          </li>
+          <li className="mb-2">
+            <a
+              href="#"
+              onClick={() => handleItemClick("Alamat")}
+              className={`cursor-pointer ${
+                selectedItem === "Alamat" ? "underline" : ""
+              }`}
+            >
+              Alamat
+            </a>
+          </li>
+          <li className="mb-2">
+            <a
+              href="#"
+              onClick={() => handleItemClick("Keluar")}
+              className={`cursor-pointer ${
+                selectedItem === "Keluar" ? "underline" : ""
+              }`}
+            >
+              Keluar
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="w-3/4 p-4">
+        <div className="content">{renderContent()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardUser;
