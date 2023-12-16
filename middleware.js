54c6b9e4fd5013529b0e01dee161096137352b0c
krisnasaputtra <krisna.saputra031002@gmail.com>
2023-12-16 17:23:15 +0700

    Fix Bug in Middleware

--- middleware.js
+++ middleware.js
@@ -1,26 +1,32 @@
 import { getCookie } from "cookies-next";
+import { parseJwt } from "./utils/parseJwt";
 import { NextResponse } from "next/server";
 
 export default function middleware(req) {
   const token = getCookie("token", { req });
-  const role = getCookie("role", { req });
+  let role = undefined;
+  if (token) {
+    const user = parseJwt(token);
+    role = user.level;
+  }
+
   const isAuthenticated = token === undefined ? false : true;
   const userRole = role === undefined ? "" : role;
   let url = req.url;
 
   if (userRole !== "admin" && url.includes("/dashboard/admin")) {
-    return NextResponse.redirect("https://dwp-frontend.vercel.app/auth/login");
+    return NextResponse.redirect("https://dwpitera.my.id/auth/login");
   }
 
   if (userRole !== "user" && url.includes("/dashboard/user")) {
-    return NextResponse.redirect("https://dwp-frontend.vercel.app/auth/login");
+    return NextResponse.redirect("https://dwpitera.my.id/auth/login");
   }
 
   if (userRole !== "penjual" && url.includes("/dashboard/penjual")) {
-    return NextResponse.redirect("https://dwp-frontend.vercel.app/auth/login");
+    return NextResponse.redirect("https://dwpitera.my.id/auth/login");
   }
 
   if (isAuthenticated && url.includes("/auth")) {
-    return NextResponse.redirect("https://dwp-frontend.vercel.app");
+    return NextResponse.redirect("https://dwpitera.my.id");
   }
 }
