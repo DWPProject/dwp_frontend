c9f5933bd59914fd6a1c43100adb602700fc691c
krisnasaputtra <krisna.saputra031002@gmail.com>
2023-12-22 08:44:26 +0700

    Update Page Heading Dashboard

--- components/dashboard/PageHeading.jsx
+++ components/dashboard/PageHeading.jsx
@@ -8,7 +8,7 @@ export default function PageHeading({ title }) {
     <div className="p-6 shadow-lg bg-white flex justify-between items-center">
       <span className="text-3xl text-[#828282] font-bold">{title}</span>
       <div className="flex gap-5 items-center">
-        <p>{user_name !== null ?? "Guest"}</p>
+        <p>{user_name}</p>
         <Image src="/logo.svg" alt="Logo DWP" width={50} height={50} />
       </div>
     </div>
